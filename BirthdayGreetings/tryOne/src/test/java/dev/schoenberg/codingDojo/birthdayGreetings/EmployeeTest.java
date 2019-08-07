34088bf5a86de8861c0729ab1c5c7cac5773b3a1
Martin Schönberg <martin@schoenberg.dev>
2019-08-07 11:56:37 +0200

    Remove useless test message

--- BirthdayGreetings/tryOne/src/test/java/dev/schoenberg/codingDojo/birthdayGreetings/EmployeeTest.java
+++ BirthdayGreetings/tryOne/src/test/java/dev/schoenberg/codingDojo/birthdayGreetings/EmployeeTest.java
@@ -8,7 +8,7 @@ public class EmployeeTest {
 	@Test
 	public void testBirthday() {
 		Employee employee = new Employee("foo", "bar", "1990/01/31", "a@b.c");
-		assertFalse("not his birthday", employee.isBirthday(new XDate("2008/01/30")));
-		assertTrue("his birthday", employee.isBirthday(new XDate("2008/01/31")));
+		assertFalse(employee.isBirthday(new XDate("2008/01/30")));
+		assertTrue(employee.isBirthday(new XDate("2008/01/31")));
 	}
 }
