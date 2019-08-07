2804a0a485567ee84bbee6284b5fd8dbb9485e8e
Martin Schönberg <martin@schoenberg.dev>
2019-08-07 11:57:00 +0200

    Refactor acceptance test

--- BirthdayGreetings/tryOne/src/test/java/dev/schoenberg/codingDojo/birthdayGreetings/AcceptanceTest.java
+++ BirthdayGreetings/tryOne/src/test/java/dev/schoenberg/codingDojo/birthdayGreetings/AcceptanceTest.java
@@ -10,8 +10,10 @@ import com.dumbster.smtp.SimpleSmtpServer;
 import com.dumbster.smtp.SmtpMessage;
 
 public class AcceptanceTest {
-
+	private static final String HOST = "localhost";
+	private static final String RESOURCE_FILE = "src/main/resources/employee_data.txt";
 	private static final int NONSTANDARD_PORT = 9999;
+
 	private BirthdayService birthdayService;
 	private SimpleSmtpServer mailServer;
 
@@ -29,23 +31,22 @@ public class AcceptanceTest {
 
 	@Test
 	public void willSendGreetings_whenItsSomebodysBirthday() {
-		birthdayService.sendGreetings("src/main/resources/employee_data.txt", new XDate("2008/10/08"), "localhost",
-				NONSTANDARD_PORT);
+		birthdayService.sendGreetings(RESOURCE_FILE, new XDate("2008/10/08"), HOST, NONSTANDARD_PORT);
 
-		assertEquals("message not sent?", 1, mailServer.getReceivedEmailSize());
+		assertEquals(1, mailServer.getReceivedEmailSize());
+
 		SmtpMessage message = (SmtpMessage) mailServer.getReceivedEmail().next();
-		assertEquals("Happy Birthday, dear John!", message.getBody());
-		assertEquals("Happy Birthday!", message.getHeaderValue("Subject"));
 		String[] recipients = message.getHeaderValues("To");
 		assertEquals(1, recipients.length);
 		assertEquals("john.doe@foobar.com", recipients[0].toString());
+		assertEquals("Happy Birthday, dear John!", message.getBody());
+		assertEquals("Happy Birthday!", message.getHeaderValue("Subject"));
 	}
 
 	@Test
 	public void willNotSendEmailsWhenNobodysBirthday() {
-		birthdayService.sendGreetings("src/main/resources/employee_data.txt", new XDate("2008/01/01"), "localhost",
-				NONSTANDARD_PORT);
+		birthdayService.sendGreetings(RESOURCE_FILE, new XDate("2008/01/01"), HOST, NONSTANDARD_PORT);
 
-		assertEquals("what? messages?", 0, mailServer.getReceivedEmailSize());
+		assertEquals(0, mailServer.getReceivedEmailSize());
 	}
 }
