b15b4fd44e690429eab859afb214cb59a95e1996
Martin Schönberg <martin@schoenberg.dev>
2019-08-07 18:44:11 +0200

    Remove useless test information

--- BirthdayGreetings/tryOne/src/test/java/dev/schoenberg/codingDojo/birthdayGreetings/core/EmployeeTest.java
+++ BirthdayGreetings/tryOne/src/test/java/dev/schoenberg/codingDojo/birthdayGreetings/core/EmployeeTest.java
@@ -4,12 +4,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import dev.schoenberg.codingDojo.birthdayGreetings.core.Employee;
-
 public class EmployeeTest {
 	@Test
 	public void testBirthday() {
-		Employee employee = new Employee("foo", "bar", "1990/01/31", "a@b.c");
+		Employee employee = new Employee("", "", "1990/01/31", "");
 		assertFalse(employee.isBirthday(new XDate("2008/01/30")));
 		assertTrue(employee.isBirthday(new XDate("2008/01/31")));
 	}
