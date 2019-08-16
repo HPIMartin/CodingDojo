729e2e949be589b69c67453d31d1a6321194cad5
Martin Schönberg <martinundpralli+bitbucket@gmail.com>
2019-08-16 22:23:49 +0200

    Use possitive logic

--- GildedRose/tryOne/src/main/java/dev/schoenberg/codingDojo/gildedRose/GildedRose.java
+++ GildedRose/tryOne/src/main/java/dev/schoenberg/codingDojo/gildedRose/GildedRose.java
@@ -10,11 +10,7 @@ public class GildedRose {
 
 	public void updateQuality() {
 		for (int i = 0; i < items.length; i++) {
-			if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-				if (items[i].quality > 0) {
-					reduceQuality(i);
-				}
-			} else {
+			if (items[i].name.equals("Aged Brie") || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 				if (items[i].quality < QUALITY_CAP) {
 					items[i].quality = items[i].quality + 1;
 
@@ -23,21 +19,25 @@ public class GildedRose {
 						increaseQualityIfLessThan6daysLeft(i);
 					}
 				}
+			} else {
+				if (items[i].quality > 0) {
+					reduceQuality(i);
+				}
 			}
 
 			reduceSellIn(i);
 
 			if (items[i].sellIn < 0) {
-				if (!items[i].name.equals("Aged Brie")) {
-					if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+				if (items[i].name.equals("Aged Brie")) {
+					increaseQuality(i);
+				} else {
+					if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+						items[i].quality = 0;
+					} else {
 						if (items[i].quality > 0) {
 							reduceQuality(i);
 						}
-					} else {
-						items[i].quality = 0;
 					}
-				} else {
-					increaseQuality(i);
 				}
 			}
 		}
